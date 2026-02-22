8ccd4fd5735bc5d09ae9a19c61447f035994e75e
villaboj-glitch <villaboj6469@student.faytechcc.edu>
2026-02-22 14:23:59 -0500

    first python assignment
    taking input from user and printing output.

---
+++ P1LAB1_Josue_VillaBonilla.py
@@ -0,0 +1,9 @@
+# Josue VillaBonilla
+# 22 Feb 2026
+# P1LAB1
+# Create a program that allows the user to enter their first name and last name.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,",first_name,last_name +"!","Welcome to CTI-110!")
